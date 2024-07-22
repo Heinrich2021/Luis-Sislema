1176c4fefc5347f671d86382475101b0ead0a38f
Luis Sislema <le.sislemag@uea.edu.ec>
2024-07-21 23:55:04 -0500

    dashboar.py

--- Semana.7POO.py
+++ Semana.7POO.py
@@ -5,9 +5,12 @@ class Marca:
         self.nombre = nombre
         self.fundacion = fundacion
         print(f"Marca '{self.nombre}' fundada en {self.fundacion}.")
-# El destructor (__del__) de Marca está configurado para imprimir un mensaje cuando se elimina la instancia de la marca.
+
+    # El destructor (__del__) de Marca está configurado para imprimir un mensaje cuando se elimina la instancia de la marca.
     def __del__(self):
         print(f"Marca '{self.nombre}' eliminada.")
+
+
 # Esta clase tiene un constructor (__init__) que inicializa el modelo de la motocicleta, el año y la instancia
 # de Marca a la que pertenece.
 # Cuando se crea una instancia de Motocicleta, se imprime un mensaje indicando la creación de la motocicleta y
@@ -18,10 +21,12 @@ class Motocicleta:
         self.anio = anio
         self.marca = marca
         print(f"Motocicleta {self.modelo} del año {self.anio} creada, de la marca '{self.marca.nombre}'.")
-# El destructor (__del__) de Motocicleta está configurado para imprimir un mensaje cuando
-# se elimina la instancia de la motocicleta.
+
+    # El destructor (__del__) de Motocicleta está configurado para imprimir un mensaje cuando
+    # se elimina la instancia de la motocicleta.
     def __del__(self):
         print(f"Motocicleta {self.modelo} del año {self.anio} eliminada.")
+
 
 # Creación de instancias de Marca y Motocicleta
 
